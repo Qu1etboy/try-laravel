8b6a1780be924f768a4bc09205807854fcf1420a
Qu1etboy <nonzagreanthai@gmail.com>
2023-07-11 13:37:33 +0700

    add laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/about', function() {
+    return "About";
+})->name('about');
+
+Route::get('/hello/{text?}', function(string $text = "World") {
+    return "Hello {$text}";
+})->name('hello-world');
+
+Route::get('/regex/{id}', function(string $id) {
+    return "Hello {$id}";
+})->where('id', '[0-9]+')->name('regex');
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// require __DIR__.'/auth.php';
